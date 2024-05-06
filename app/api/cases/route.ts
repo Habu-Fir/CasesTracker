098c3e05bea9dcdc7bc83aa46abbb5a65d157771
Habu-Fir <tohabtish16@gmail.com>
2024-05-06 13:08:31 +0300

    Building an API for crating a Case

---
+++ app/api/cases/route.ts
@@ -0,0 +1,21 @@
+import prisma from '@/prisma/client';
+import { NextRequest, NextResponse } from 'next/server';
+import { z } from 'zod';
+
+const createCaseSchema = z.object({
+  title: z.string().min(1).max(250),
+  description: z.string().min(1),
+});
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const validation = createCaseSchema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
+
+  const newCase = await prisma.case.create({
+    data: { title: body.title, description: body.description },
+  });
+  console.log(newCase);
+  return NextResponse.json(newCase, { status: 201 });
+}
