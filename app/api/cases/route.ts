3f516e8baadb79ae627b8b7668b852a3d87a6805
Habu-Fir <tohabtish16@gmail.com>
2024-05-08 10:15:49 +0300

    Handle Errors!

--- app/api/cases/route.ts
+++ app/api/cases/route.ts
@@ -3,15 +3,15 @@ import { NextRequest, NextResponse } from 'next/server';
 import { z } from 'zod';
 
 const createCaseSchema = z.object({
-  title: z.string().min(1).max(250),
-  description: z.string().min(1),
+  title: z.string().min(1, 'Title is required').max(250),
+  description: z.string().min(1, 'Description is required'),
 });
 
 export async function POST(request: NextRequest) {
   const body = await request.json();
   const validation = createCaseSchema.safeParse(body);
   if (!validation.success)
-    return NextResponse.json(validation.error.errors, { status: 400 });
+    return NextResponse.json(validation.error.format(), { status: 400 });
 
   const newCase = await prisma.case.create({
     data: { title: body.title, description: body.description },
--- app/cases/new/page.tsx
+++ app/cases/new/page.tsx
@@ -2,9 +2,10 @@
 import SimpleMDE from 'react-simplemde-editor';
 import 'easymde/dist/easymde.min.css';
 import { useForm, Controller } from 'react-hook-form';
-import { Button, TextField } from '@radix-ui/themes';
+import { Button, Callout, TextField } from '@radix-ui/themes';
 import axios from 'axios';
 import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 interface CaseForm {
   title: string;
   description: string;
@@ -13,29 +14,43 @@ interface CaseForm {
 const NewCasePage = () => {
   const { register, control, handleSubmit } = useForm<CaseForm>();
   const router = useRouter();
+  const [error, setError] = useState('');
+
   return (
-    <form
-      className="max-w-xl space-y-4"
-      onSubmit={handleSubmit(async (data) => {
-        await axios.post('/api/cases', data);
-        router.push('/cases');
-      })}
-    >
-      <TextField.Root
-        // color="indigo"
-        variant="soft"
-        placeholder="Title of the case"
-        {...register('title')}
-      />
-      <Controller
-        name="description"
-        control={control}
-        render={({ field }) => (
-          <SimpleMDE placeholder="Description" {...field} />
-        )}
-      />
-      <Button>Submit</Button>
-    </form>
+    <div className="max-w-xl">
+      {error && (
+        <Callout.Root color="red" className="mb-5">
+          <Callout.Text></Callout.Text>
+          {error}
+        </Callout.Root>
+      )}
+      <form
+        className=" space-y-4"
+        onSubmit={handleSubmit(async (data) => {
+          try {
+            await axios.post('/api/cases', data);
+            router.push('/cases');
+          } catch (error) {
+            setError('Un Expected error occur!');
+          }
+        })}
+      >
+        <TextField.Root
+          // color="indigo"
+          variant="soft"
+          placeholder="Title of the case"
+          {...register('title')}
+        />
+        <Controller
+          name="description"
+          control={control}
+          render={({ field }) => (
+            <SimpleMDE placeholder="Description" {...field} />
+          )}
+        />
+        <Button>Submit</Button>
+      </form>
+    </div>
   );
 };
 
