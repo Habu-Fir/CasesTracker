f6a65c21d9baed94fb2dfa67e520381015747452
Habu-Fir <tohabtish16@gmail.com>
2024-05-08 09:12:12 +0300

    Handle form Submit

--- app/cases/new/page.tsx
+++ app/cases/new/page.tsx
@@ -1,19 +1,41 @@
 'use client';
 import SimpleMDE from 'react-simplemde-editor';
 import 'easymde/dist/easymde.min.css';
+import { useForm, Controller } from 'react-hook-form';
 import { Button, TextField } from '@radix-ui/themes';
+import axios from 'axios';
+import { useRouter } from 'next/navigation';
+interface CaseForm {
+  title: string;
+  description: string;
+}
 
 const NewCasePage = () => {
+  const { register, control, handleSubmit } = useForm<CaseForm>();
+  const router = useRouter();
   return (
-    <div className="max-w-xl space-y-4">
+    <form
+      className="max-w-xl space-y-4"
+      onSubmit={handleSubmit(async (data) => {
+        await axios.post('/api/cases', data);
+        router.push('/cases');
+      })}
+    >
       <TextField.Root
         // color="indigo"
         variant="soft"
         placeholder="Title of the case"
+        {...register('title')}
       />
-      <SimpleMDE placeholder="Description of the case" />
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="Description" {...field} />
+        )}
+      />
       <Button>Submit</Button>
-    </div>
+    </form>
   );
 };
 
